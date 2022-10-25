96832c66bff1f5451dcaf72a295441291de1d7e9
Roberto Carrillo <roberto_car_av@hotmail.com>
2022-10-25 00:51:45 -0500

    add navbar component

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,11 +3,11 @@ import "./Navbar.scss"
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <img className="navbar-item navbar-icon-left" src="/assets/icons/menu.svg" alt="hamburger menu" />
+      <img className="navbar-item navbar-icon-left" src="/assets/icons/menu_white.svg" alt="hamburger menu" />
       <h1 className="navbar-item navbar-title">TARGET</h1>
-      <img className="navbar-item navbar-icon-right" src="/assets/icons/pin.svg" alt="pin" />
+      <img className="navbar-item navbar-icon-right" src="/assets/icons/pin_white.svg" alt="pin" />
     </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
