3b39d706fcc99597e456b8e76fcc6f6570aa07dc
Roberto Carrillo <roberto_car_av@hotmail.com>
2022-10-25 02:46:20 -0500

    fix links

--- src/components/LandingVideo/LandingVideo.js
+++ src/components/LandingVideo/LandingVideo.js
@@ -5,19 +5,19 @@ const LandingVideo = () => {
     <div className="landing-video">
       <img id="preview" src="/assets/iphone6.png" alt="iPhone6" />
 
-      <a href="#" className="appstore">
+      <a href="https://www.apple.com/app-store/" className="appstore">
         <img src="/assets/appstore.png" alt="App Store" />
       </a>
 
       <ul className="social">
         <li className="social-item">
-          <a href="#">
+          <a href="https://www.facebook.com/">
             <img src="/assets/icons/facebook.svg" alt="Facebook" />
           </a>
         </li>
 
         <li className="social-item">
-          <a href="#">
+          <a href="https://twitter.com/">
             <img src="/assets/icons/twitter.svg" alt="Twitter" />
           </a>
         </li>
