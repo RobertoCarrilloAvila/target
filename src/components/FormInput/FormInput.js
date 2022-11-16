f76a0558f958111111c0b85d092108f4b8645982
Roberto Carrillo <roberto_car_av@hotmail.com>
2022-11-15 18:49:21 -0600

    destructure form input params

--- src/components/FormInput/FormInput.js
+++ src/components/FormInput/FormInput.js
@@ -1,23 +1,23 @@
 import "./FormInput.scss";
 
-const FormInput = (props) => {
+const FormInput = ({ id, label, type, value, name, placeholder = "", onChange}) => {
   const handleChange = (event) => {
     const text = event.target.value;
-    props.onChange(text);
+    onChange(text);
   };
 
   return (
     <div className="form-input">
-      <label className="form-input-label" htmlFor={props.id}>
-        {props.label}
+      <label className="form-input-label" htmlFor={id}>
+        {label}
       </label>
       <input
         className="form-input-input"
-        type={props.type}
-        value={props.value}
-        name={props.name}
-        id={props.id}
-        placeholder={props.placeholder}
+        type={type}
+        value={value}
+        name={name}
+        id={id}
+        placeholder={placeholder}
         onChange={handleChange}
         onBlur={handleChange}
       />
