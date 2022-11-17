a591224efe9aea1226fa06b85c3338e858fcbd06
Roberto Carrillo <roberto_car_av@hotmail.com>
2022-11-17 17:20:30 -0600

    add signup component

---
+++ src/components/SignUp/SignUp.js
@@ -0,0 +1,54 @@
+import Navbar from "components/Navbar/Navbar";
+import FormInput from "components/FormInput/FormInput";
+import LandingVideo from "components/LandingVideo/LandingVideo";
+import "./SignUp.scss";
+
+const SignUp = () => {
+  return (
+    <div className="sign-up">
+      <section className="container">
+        <Navbar className="white" />
+
+        <h1 id="sign-up-title">Sign Up</h1>
+
+        <form className="sign-up-form">
+          <FormInput
+            type="text"
+            id="name"
+            name="name"
+            label="Name"
+          />
+
+          <FormInput
+            type="email"
+            id="email"
+            name="email"
+            label="Email"
+          />
+
+          <FormInput
+            type="password"
+            id="password"
+            name="password"
+            label="Password"
+          />
+
+          <FormInput
+            type="password"
+            id="password_confirmation"
+            name="password_confirmation"
+            label="Confirm Password"
+          />
+
+          <button type="submit" id="sign-up-submit" className="btn">
+            Sign Up
+          </button>
+        </form>
+      </section>
+
+      <LandingVideo />
+    </div>
+  );
+};
+
+export default SignUp;
