e3c85cb28e9022d3cf397c1ef51a19d7876f5d58
Roberto Carrillo <roberto_car_av@hotmail.com>
2022-11-18 15:32:38 -0600

    add routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.scss";
 import Login from "./components/Login/Login";
+import SignUp from "components/SignUp/SignUp";
 
 function App() {
-  return <Login />;
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/sign_up" element={<SignUp />} />
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
