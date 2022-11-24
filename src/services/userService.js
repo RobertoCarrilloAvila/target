7344b41639e22c44415da1360e27680df02efa76
Roberto Carrillo <roberto_car_av@hotmail.com>
2022-11-24 15:25:34 -0600

    add sign up service

--- src/services/userService.js
+++ src/services/userService.js
@@ -5,6 +5,10 @@ const UserService = {
     const { data } = await client.post("/users/sign_in", request);
     return data;
   },
+  signUp: async (request) => {
+    const { data } = await client.post("/users", request);
+    return data;
+  }
 };
 
 export default UserService;
