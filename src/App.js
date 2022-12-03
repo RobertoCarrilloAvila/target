f7256a7cace6bd2412eb6f51418e63457f248603
Roberto Carrillo <roberto_car_av@hotmail.com>
2022-12-02 18:00:28 -0600

    define url

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
 import Login from "./components/Login/Login";
 import SignUp from "components/SignUp/SignUp";
+import About from "components/About/About";
 
 function App() {
   return(
     <BrowserRouter>
       <Routes>
         <Route path="/sign_up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
         <Route path="/" element={<Login />} />
       </Routes>
     </BrowserRouter>
