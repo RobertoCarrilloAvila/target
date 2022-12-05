2baab3b5fe2c4d821f78186698b0f274c63a71a9
Roberto Carrillo <roberto_car_av@hotmail.com>
2022-12-05 15:40:00 -0600

    remove else statemen

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -46,81 +46,81 @@ const SignUp = () => {
 
   if (success) {
     return <SignUpConfirm />;
-  } else {
-    return (
-      <div className="sign-up">
-        <section className="container">
-          <Navbar className="white" />
-  
-          <h1 id="sign-up-title">Sign Up</h1>
-  
-          <form className="sign-up-form" onSubmit={handleSubmit}>
-            <FormInput
-              type="text"
-              id="name"
-              name="name"
-              label="name"
-              value={name}
-              onChange={setName}
-              required={true}
-            />
-  
-            <FormInput
-              type="email"
-              id="email"
-              name="email"
-              label="email"
-              value={email}
-              onChange={setEmail}
-              required={true}
-            />
-  
-            <FormInput
-              type="password"
-              id="password"
-              name="password"
-              label="password"
-              placeholder="min. 6 characters long"
-              value={password}
-              onChange={setPassword}
-              minLength="6"
-              required={true}
-            />
-  
-            <FormInput
-              type="password"
-              id="password_confirmation"
-              name="password_confirmation"
-              label="confirm password"
-              value={passwordConfirmation}
-              onChange={setPasswordConfirmation}
-              minLength="6"
-              required={true}
-            />
-  
-            <FormSelect
-              id="gender"
-              name="gender"
-              label="gender"
-              options={GENDER_OPTIONS}
-              placeholder="select your gender"
-              value={gender}
-              onChange={setGender}
-              required={true}
-            />
-  
-            <button type="submit" id="sign-up-submit" className="btn">
-              Sign Up
-            </button>
-          </form>
-  
-          <Link to="/" className="email-login">Sign In</Link>
-        </section>
-  
-        <LandingVideo />
-      </div>
-    );
   }
+
+  return (
+    <div className="sign-up">
+      <section className="container">
+        <Navbar className="white" />
+
+        <h1 id="sign-up-title">Sign Up</h1>
+
+        <form className="sign-up-form" onSubmit={handleSubmit}>
+          <FormInput
+            type="text"
+            id="name"
+            name="name"
+            label="name"
+            value={name}
+            onChange={setName}
+            required={true}
+          />
+
+          <FormInput
+            type="email"
+            id="email"
+            name="email"
+            label="email"
+            value={email}
+            onChange={setEmail}
+            required={true}
+          />
+
+          <FormInput
+            type="password"
+            id="password"
+            name="password"
+            label="password"
+            placeholder="min. 6 characters long"
+            value={password}
+            onChange={setPassword}
+            minLength="6"
+            required={true}
+          />
+
+          <FormInput
+            type="password"
+            id="password_confirmation"
+            name="password_confirmation"
+            label="confirm password"
+            value={passwordConfirmation}
+            onChange={setPasswordConfirmation}
+            minLength="6"
+            required={true}
+          />
+
+          <FormSelect
+            id="gender"
+            name="gender"
+            label="gender"
+            options={GENDER_OPTIONS}
+            placeholder="select your gender"
+            value={gender}
+            onChange={setGender}
+            required={true}
+          />
+
+          <button type="submit" id="sign-up-submit" className="btn">
+            Sign Up
+          </button>
+        </form>
+
+        <Link to="/" className="email-login">Sign In</Link>
+      </section>
+
+      <LandingVideo />
+    </div>
+  );
 };
 
 export default SignUp;
