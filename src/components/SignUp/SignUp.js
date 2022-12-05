4ee4cb4acba15a459d4d959ea33776f4bf421bb1
Roberto <roberto_car_av@hotmail.com>
2022-12-05 15:43:17 -0600

    Signup confirmation (#7)
    * Add prettier
    
    * Fix style
    
    * set scss
    
    * Add navbar
    
    * set global scss
    
    * add navbar component
    
    * Add login view
    
    * fix box shadow
    
    * navbar icon color
    
    * Adjust navbar
    
    * add landing video
    
    * fix navbar media query
    
    * fix links
    
    * add sign up buttons
    
    * prettier
    
    * add login request
    
    * move assets folder
    
    * move login logic into a service
    
    * format
    
    * add FormInput tests
    
    * Add navbar tests
    
    * add test for landing video
    
    * format
    
    * destructure form input params
    
    * validate if label prop is empty
    
    * remove logs
    
    * Fix css
    
    * move base url
    
    * absolute imports
    
    * improve css
    
    * remove unneeded css
    
    * add signup component
    
    * add routes
    
    * use Link component
    
    * add form select component
    
    * add sign up service
    
    * add signup component
    
    * fix linter
    
    * add signup tests
    
    * fix iphone chrome select
    
    * add semibold variable
    
    * fix login color
    
    * set navbar height
    
    * add SignUp confirm component
    
    * display sign up confirm component
    
    * add tests
    
    * remove else statemen

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,4 +1,4 @@
-import { Link, Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useState } from "react";
 
 import UserService from "../../services/userService";
@@ -6,6 +6,7 @@ import Navbar from "components/Navbar/Navbar";
 import FormInput from "components/FormInput/FormInput";
 import FormSelect from "components/FormSelect/FormSelect";
 import LandingVideo from "components/LandingVideo/LandingVideo";
+import SignUpConfirm from "components/SignUpConfirm/SignUpConfirm";
 
 import "./SignUp.scss";
 
@@ -17,7 +18,7 @@ const SignUp = () => {
   const [password, setPassword] = useState("");
   const [passwordConfirmation, setPasswordConfirmation] = useState("");
   const [gender, setGender] = useState("");
-  const [redirect, setRedirect] = useState(false);
+  const [success, setSuccess] = useState(false);
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -34,94 +35,92 @@ const SignUp = () => {
       }
     };
 
-
     try {
       await UserService.signUp(body);
-      setRedirect(true);
+      setSuccess(true);
     } catch (error) {
       const { response: {data: {errors}} } = error;
       alert(errors.full_messages[0]);
     }
   };
 
-  if (redirect) {
-    return <Navigate to="/" />
-  }else{
-
-    return (
-      <div className="sign-up">
-        <section className="container">
-          <Navbar className="white" />
-  
-          <h1 id="sign-up-title">Sign Up</h1>
-  
-          <form className="sign-up-form" onSubmit={handleSubmit}>
-            <FormInput
-              type="text"
-              id="name"
-              name="name"
-              label="name"
-              value={name}
-              onChange={setName}
-              required={true}
-            />
-  
-            <FormInput
-              type="email"
-              id="email"
-              name="email"
-              label="email"
-              value={email}
-              onChange={setEmail}
-              required={true}
-            />
-  
-            <FormInput
-              type="password"
-              id="password"
-              name="password"
-              label="password"
-              placeholder="min. 6 characters long"
-              value={password}
-              onChange={setPassword}
-              minLength="6"
-              required={true}
-            />
-  
-            <FormInput
-              type="password"
-              id="password_confirmation"
-              name="password_confirmation"
-              label="confirm password"
-              value={passwordConfirmation}
-              onChange={setPasswordConfirmation}
-              minLength="6"
-              required={true}
-            />
-  
-            <FormSelect
-              id="gender"
-              name="gender"
-              label="gender"
-              options={GENDER_OPTIONS}
-              placeholder="select your gender"
-              value={gender}
-              onChange={setGender}
-              required={true}
-            />
-  
-            <button type="submit" id="sign-up-submit" className="btn">
-              Sign Up
-            </button>
-          </form>
-  
-          <Link to="/" className="email-login">Sign In</Link>
-        </section>
-  
-        <LandingVideo />
-      </div>
-    );
+  if (success) {
+    return <SignUpConfirm />;
   }
+
+  return (
+    <div className="sign-up">
+      <section className="container">
+        <Navbar className="white" />
+
+        <h1 id="sign-up-title">Sign Up</h1>
+
+        <form className="sign-up-form" onSubmit={handleSubmit}>
+          <FormInput
+            type="text"
+            id="name"
+            name="name"
+            label="name"
+            value={name}
+            onChange={setName}
+            required={true}
+          />
+
+          <FormInput
+            type="email"
+            id="email"
+            name="email"
+            label="email"
+            value={email}
+            onChange={setEmail}
+            required={true}
+          />
+
+          <FormInput
+            type="password"
+            id="password"
+            name="password"
+            label="password"
+            placeholder="min. 6 characters long"
+            value={password}
+            onChange={setPassword}
+            minLength="6"
+            required={true}
+          />
+
+          <FormInput
+            type="password"
+            id="password_confirmation"
+            name="password_confirmation"
+            label="confirm password"
+            value={passwordConfirmation}
+            onChange={setPasswordConfirmation}
+            minLength="6"
+            required={true}
+          />
+
+          <FormSelect
+            id="gender"
+            name="gender"
+            label="gender"
+            options={GENDER_OPTIONS}
+            placeholder="select your gender"
+            value={gender}
+            onChange={setGender}
+            required={true}
+          />
+
+          <button type="submit" id="sign-up-submit" className="btn">
+            Sign Up
+          </button>
+        </form>
+
+        <Link to="/" className="email-login">Sign In</Link>
+      </section>
+
+      <LandingVideo />
+    </div>
+  );
 };
 
 export default SignUp;
