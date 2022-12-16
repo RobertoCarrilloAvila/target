4e726e6bdf761232b912326769bfa6f9950ef43d
Roberto Carrillo <roberto_car_av@hotmail.com>
2022-12-16 13:38:23 -0600

    add setuo for test

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,33 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next'
+
+// Mock t function
+export const t = (key, params) => {
+  if (key === 'key.with.params') {
+    return `key.with.params.${params.param}`
+  }
+
+  return key
+}
+
+// Mock react-i18next
+// i18n.use(initReactI18next).init({
+//   fallbackLng: 'en',
+//   debug: true,
+//   interpolation: {
+//     escapeValue: false, // not needed for react as it escapes by default
+//   }
+// });
+
+// Mock your i18n
+jest.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: (str) => str,
+    };
+  },
+}));
