4c14854d1bfad6a2f36cd060671f94d73b5ad576
Roberto Carrillo <roberto_car_av@hotmail.com>
2023-09-11 02:33:44 -0600

    call modal component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,14 +4,21 @@ import menuBlack from "../../assets/icons/menu_black.svg";
 import menuWhite from "../../assets/icons/menu_white.svg";
 import pinBlack from "../../assets/icons/pin_black.svg";
 import pinWhite from "../../assets/icons/pin_white.svg";
+import { useState } from "react";
+import Modal from "components/Modal/Modal";
 
 const Navbar = ({ className }) => {
+  const [showmenu, setShowMenu] = useState(false);
+  const [showContactModal, setShowContactModal] = useState(false);
   const blueNavbar = className.includes("blue");
 
-  const toggleMenu = (event) => {
-    const target_id = event.currentTarget.dataset.target;
-    const menu = document.getElementById(target_id);
-    menu.classList.toggle("show");
+  const toggleMenu = () => {
+    setShowMenu(!showmenu);
+  };
+
+  const toggleContactModal = () => {
+    setShowMenu(false);
+    setShowContactModal(!showContactModal);
   };
 
   return (
@@ -24,16 +31,28 @@ const Navbar = ({ className }) => {
         />
       </button>
 
-      <div id="navbar-collapsible-menu">
-        <ul className="navbar-menu">
-          <li className="navbar-menu-item">
-            <Link to="/about" className="navbar-link">about</Link>
-          </li>
-          <li className="navbar-menu-item">
-            <a href="#contact" className="navbar-link">contact</a>
-          </li>
-        </ul>
-      </div>
+      {
+        showmenu ? (
+          <div id="navbar-collapsible-menu">
+            <ul className="navbar-menu">
+              <li className="navbar-menu-item">
+                <Link to="/about" className="navbar-link">about</Link>
+              </li>
+              <li className="navbar-menu-item">
+                <button onClick={toggleContactModal} className="navbar-link">contact</button>
+              </li>
+            </ul>
+          </div>
+        ) : null
+      }
+
+      {
+        showContactModal ? (
+          <Modal toggleModal={toggleContactModal}>
+            
+          </Modal>
+        ) : null
+      }
 
       <h1 className="navbar-item navbar-title">TARGET</h1>
 
