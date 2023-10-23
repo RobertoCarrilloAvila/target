4bbb4c8c2884f5a6afdc90310c8db88e65730131
Roberto Carrillo <roberto_car_av@hotmail.com>
2023-10-23 02:00:47 -0600

    order paths

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import PublicPaths from "components/Constants/PublicPaths";
+import PrivatePaths from "components/Constants/PrivatePaths";
+
 import "./App.scss";
 import Login from "./components/Login/Login";
 import SignUp from "components/SignUp/SignUp";
 import About from "components/About/About";
+import Welcome from "components/Welcome/Welcome";
 
 function App() {
   return(
     <BrowserRouter>
       <Routes>
-        <Route path="/sign_up" element={<SignUp />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/" element={<Login />} />
+        <Route path={PublicPaths.ROOT} element={<Login />} />
+        <Route path={PublicPaths.ABOUT} element={<About />} />
+        <Route path={PublicPaths.SIGN_UP} element={<SignUp />} />
+
+        <Route path={PrivatePaths.HOME} element={<Welcome />} />
       </Routes>
     </BrowserRouter>
   );
