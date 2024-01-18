23763ce17c9a922c9b41e3f9bb51a9e660f824fb
Roberto Carrillo <roberto_car_av@hotmail.com>
2024-01-18 00:21:11 -0600

    fix async request

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -15,7 +15,6 @@ const Navbar = ({ className }) => {
   const [showmenu, setShowMenu] = useState(false);
   const [showContactModal, setShowContactModal] = useState(false);
   const [isLoggedIn] = useState(UserService.isLoggedIn());
-
   const navigate = useNavigate();
   const blueNavbar = className.includes("blue");
 
@@ -28,8 +27,8 @@ const Navbar = ({ className }) => {
     setShowContactModal(!showContactModal);
   };
 
-  const handleLogout = () => {
-    if (UserService.logOut()) {
+  const handleLogout = async () => {
+    if (await UserService.logOut()) {
       navigate(PublicPaths.ROOT);
     } else {
       alert("Something went wrong. Please try again.");
