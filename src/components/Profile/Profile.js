399dacfda91edc7d50c74559b2be09da9b3e7b9e
Roberto Carrillo <roberto_car_av@hotmail.com>
2024-01-22 00:21:02 -0600

    build profile

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+
+import 'components/Profile/Profile.scss';
+import profile from 'assets/profile.png';
+
+const Profile = () => {
+  return (
+    <div className="profile">
+      <div className="profile__container">
+        <img src={profile} alt="avatar" className="profile__avatar" />
+        <h1 className="profile__name">cbrum</h1>
+        <div className='profile__actions'>
+          <span className='profile__edit'>Edit</span>
+          <span className='profile__slash'>/</span>
+          <span className='profile__logout'>Logout</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile;
