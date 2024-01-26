b862278e177edabf25d183e1784e7f357a3efdad
Roberto Carrillo <roberto_car_av@hotmail.com>
2024-01-25 21:24:38 -0600

    implement new react lib for maps

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,24 +1,46 @@
-import { useState, useEffect } from "react";
-import { createMap, setCurrentLocationAsCenter } from "services/MapService";
+import { useState, useEffect } from 'react';
+import { GoogleMap, useLoadScript } from '@react-google-maps/api';
 import "components/Map/Map.scss";
 
-const MAP_ID = "map";
+const defaultLocation = { lat: 19.432, lng: -99.133 };
+const defaultZoom = 12;
 
 const Map = () => {
-  const [map, setMap] = useState(null);
+  const [currentLocation, setCurrentLocation] = useState(defaultLocation);
+  
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+  });
 
   useEffect(() => {
-    const initializeMap = async () => {
-      setMap(await createMap("map"));
-      setCurrentLocationAsCenter(map);
-    };
-
-    initializeMap();
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setCurrentLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        });
+      });
+    }
   }, []);
 
+  if (loadError) {
+    return <div className='map__error'>Error loading maps</div>;
+  }
+
+  if (!isLoaded) {
+    return <div className='map__loading'>Loading maps</div>;
+  }
+
   return (
-    <div id={ MAP_ID }></div>
-  );
+    <div className="map">
+      <GoogleMap
+        mapContainerClassName="map__container"
+        center={currentLocation}
+        zoom={defaultZoom} >
+
+      </GoogleMap>
+    </div>
+  )
 }
 
 export default Map;
