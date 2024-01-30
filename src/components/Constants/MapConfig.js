ec5f9fd6410db8804ebdbf4e07b3c2e6c2f00d6d
Roberto Carrillo <roberto_car_av@hotmail.com>
2024-01-30 15:17:41 -0600

    add marker

--- src/components/Constants/MapConfig.js
+++ src/components/Constants/MapConfig.js
@@ -1,6 +1,10 @@
 const MapConfig = {
   defaultLocation: { lat: 19.432, lng: -99.133 },
   defaultZoom: 12,
+  options: {
+    draggableCursor: 'default'
+  },
+  markerIcon: { scaledSize: new window.google.maps.Size(44, 52) }
 };
 
 export default MapConfig;
