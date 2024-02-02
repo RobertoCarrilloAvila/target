1bb5b58d6f4a02ac60a3658daa021a7f11e53404
Roberto Carrillo <roberto_car_av@hotmail.com>
2024-02-02 00:38:11 -0600

    display circles

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,6 +1,7 @@
 import { useState, useEffect, useContext } from 'react';
-import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api';
+import { GoogleMap, Marker, useLoadScript, Circle } from '@react-google-maps/api';
 
+import TargetsService from 'services/TargetsService';
 import MapConfig from 'components/Constants/MapConfig';
 import MapContext from 'contexts/MapContext';
 
@@ -8,7 +9,7 @@ import 'components/Map/Map.scss';
 import pin from 'assets/map/pin.png';
 
 const Map = ({ onSelectLocation }) => {
-  const { selectedLocation, setSelectedLocation } = useContext(MapContext);
+  const { selectedLocation, setSelectedLocation, selectedRadius, targets, setTargets } = useContext(MapContext);
   const [currentLocation, setCurrentLocation] = useState(
     MapConfig.defaultLocation
   );
@@ -18,13 +19,23 @@ const Map = ({ onSelectLocation }) => {
   });
 
   useEffect(() => {
+    fetchCurrentLocation();
+    fetchTargets();
+  }, []);
+
+  const fetchCurrentLocation = () => {
     navigator.geolocation.getCurrentPosition((position) => {
       setCurrentLocation({
         lat: position.coords.latitude,
         lng: position.coords.longitude,
       });
     });
-  }, []);
+  };
+
+  const fetchTargets = async () => {
+    const response = await TargetsService.getTargets();
+    setTargets(response);
+  }
 
   const handleMapClick = (e) => {
     setSelectedLocation({ lat: e.latLng.lat(), lng: e.latLng.lng() });
@@ -51,8 +62,20 @@ const Map = ({ onSelectLocation }) => {
         onClick={(e) => handleMapClick(e)}
       >
         {selectedLocation && (
-          <Marker position={selectedLocation} icon={{ url: pin }} />
+          <>
+            <Marker position={selectedLocation} icon={{ url: pin }} />
+            <Circle center={selectedLocation} radius={selectedRadius} options={MapConfig.selectedLocationOptions} />
+          </>
         )}
+
+        {targets.map(({ target: { id, latitude, longitude, radius }} ) => (
+          <Circle
+            key={id}
+            center={{ lat: latitude, lng: longitude }}
+            radius={radius}
+            options={MapConfig.targetsOptions}
+          />
+        ))}
       </GoogleMap>
     </div>
   );
