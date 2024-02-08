93489056435e0f49953505b39dafcb25c65b26b3
Roberto Carrillo <roberto_car_av@hotmail.com>
2024-02-08 17:58:55 -0600

    open edit profile component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,16 +1,29 @@
+import { useContext } from 'react';
+import ContentViewContext from 'contexts/ContentViewContext';
 import 'components/Profile/Profile.scss';
 import profile from 'assets/profile.png';
 
 const Profile = () => {
+  const { setDisplayedComponent } = useContext(ContentViewContext);
+
   return (
     <div className="profile">
       <div className="profile__container">
         <img src={profile} alt="avatar" className="profile__avatar" />
         <h1 className="profile__name">cbrum</h1>
         <div className="profile__actions">
-          <span className="profile__edit">Edit</span>
+          <button
+            className="profile__edit profile__btn"
+            onClick={() => setDisplayedComponent('EditProfile')}
+          >
+            Edit
+          </button>
           <span className="profile__slash">/</span>
-          <span className="profile__logout">Logout</span>
+          <button
+            className="profile__logout profile__btn"
+          >
+            Logout
+          </button>
         </div>
       </div>
     </div>
