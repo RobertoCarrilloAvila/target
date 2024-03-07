a21b230643b1f0e5d5ed5e4571499170a9e10aaa
Roberto Carrillo <roberto_car_av@hotmail.com>
2024-03-07 01:41:48 -0600

    create target component

--- src/components/CreateTarget/CreateTarget.js
+++ src/components/CreateTarget/CreateTarget.js
@@ -1,8 +1,9 @@
 import { useState } from 'react';
+import { toast } from 'react-toastify';
+import { useTranslation } from 'react-i18next';
+
 import useContentView from 'hooks/useContentView';
 import useMap from 'hooks/useMap';
-import { toast } from 'react-toastify';
-
 import TargetsService from 'services/TargetsService';
 import FormInput from 'components/FormInput/FormInput';
 import FormSelect from 'components/FormSelect/FormSelect';
@@ -15,6 +16,7 @@ import 'components/CreateTarget/CreateTarget.scss';
 const MAX_TARGETS_AMOUNT = 10;
 
 const CreateTarget = () => {
+  const { t } = useTranslation();
   const {
     setMapProperties,
     selectedLocation,
@@ -45,7 +47,7 @@ const CreateTarget = () => {
   const createTarget = async (event) => {
     event.preventDefault();
     if (targets.length >= MAX_TARGETS_AMOUNT) {
-      toast.error('You have reached the maximum amount of targets');
+      toast.error(t('createTarget.limitReached'));
       return;
     }
 
@@ -66,7 +68,7 @@ const CreateTarget = () => {
     if (target) {
       goTo(COMPONENT_NAMES.CHAT);
     } else {
-      alert('Error creating target');
+      alert(t('createTarget.createError'));
     }
   };
 
@@ -78,7 +80,7 @@ const CreateTarget = () => {
       );
       goTo(COMPONENT_NAMES.CHAT);
     } else {
-      alert('Error deleting target');
+      alert(t('createTarget.deleteError'));
     }
   };
 
@@ -98,14 +100,14 @@ const CreateTarget = () => {
 
       <div className="create-target__container">
         <img src={target} alt="target" className="create-target__icon" />
-        <h1 className="create-target__title">create new target</h1>
+        <h1 className="create-target__title">{t('createTarget.title')}</h1>
 
         <form className="create-target__form" onSubmit={createTarget}>
           <FormInput
             type="number"
             name="radius"
             className="create-target__input"
-            label="specify area length"
+            label={t('createTarget.radiusLabel')}
             required
             onChange={(val) => {
               setMapProperties({
@@ -121,7 +123,7 @@ const CreateTarget = () => {
             type="text"
             name="title"
             className="create-target__input"
-            label="target title"
+            label={t('createTarget.titleLabel')}
             required
             onChange={setTitle}
             value={title}
@@ -131,7 +133,7 @@ const CreateTarget = () => {
             options={topicsList}
             name="topic"
             className="create-target__input"
-            label="select a topic"
+            label={t('createTarget.topicLabel')}
             required
             placeholder=""
             onChange={setTopicId}
@@ -144,7 +146,7 @@ const CreateTarget = () => {
               className="create-target__delete btn create-target__btn-primary"
               onClick={deleteTarget}
             >
-              delete
+              {t('createTarget.deleteButton')}
             </button>
           ) : (
             <button
@@ -152,7 +154,7 @@ const CreateTarget = () => {
               className="create-target__submit btn create-target__btn-primary"
               disabled={!selectedRadius || !title || !topicId}
             >
-              save target
+              {t('createTarget.saveButton')}
             </button>
           )}
         </form>
