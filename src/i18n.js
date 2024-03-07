213e94f8b60069505ff441b188a81d8283dab57a
Roberto Carrillo <roberto_car_av@hotmail.com>
2024-03-07 04:03:32 -0600

    fix i18n config

--- src/i18n.js
+++ src/i18n.js
@@ -1,19 +1,22 @@
 import i18n, { use } from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import Backend from 'i18next-http-backend';
+// import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-use(Backend)
-  .use(LanguageDetector)
+import en from 'locales/en/translation.json';
+
+use(LanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: 'en',
     debug: process.env.NODE_ENV === 'development',
-
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
+    resources: { 
+      en: { translation: en }
+    }
   });
 
 export default i18n;
