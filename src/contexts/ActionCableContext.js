a8b10d923a04809cf8ee8bd5a63158c770fc5e52
Roberto Carrillo <roberto_car_av@hotmail.com>
2024-03-06 21:56:10 -0600

    add useMemo

--- src/contexts/ActionCableContext.js
+++ src/contexts/ActionCableContext.js
@@ -1,18 +1,18 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect, useMemo } from 'react';
 import * as ActionCable from '@rails/actioncable';
 
 import UserService from 'services/UserService';
 
 const ActionCableContext = createContext();
 
-const socketUrl = () => {
-  const { id: userId, email } = UserService.userData() || {};
-  const urlParams = new URLSearchParams({ user: userId, uid: email });
-  return `${process.env.REACT_APP_ACTIONCABLE_HOST}?${urlParams}`;
-};
-
 const ActionCableContextProvider = ({ children }) => {
   const [cable, setCable] = useState(null);
+  const { id: userId, email } = UserService.userData() || {};
+
+  const socketUrl = useMemo(() => {
+    const urlParams = new URLSearchParams({ user: userId, uid: email });
+    return `${process.env.REACT_APP_ACTIONCABLE_HOST}?${urlParams}`;
+  }, [userId, email]);
 
   useEffect(() => {
     const cable = ActionCable.createConsumer(socketUrl());
@@ -21,7 +21,7 @@ const ActionCableContextProvider = ({ children }) => {
     return () => {
       if (cable) cable.disconnect();
     };
-  }, []);
+  }, [socketUrl]);
 
   return (
     <ActionCableContext.Provider value={{ cable, setCable }}>
