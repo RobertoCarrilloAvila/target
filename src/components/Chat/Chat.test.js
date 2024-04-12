a6b576f06f74790e47c1822fa2d68c47245dff9f
Roberto Carrillo <roberto_car_av@hotmail.com>
2024-04-12 05:09:39 -0600

    Fix format

--- src/components/Chat/Chat.test.js
+++ src/components/Chat/Chat.test.js
@@ -8,24 +8,28 @@ const mock = new MockAdapter(httpClient);
 const conversations = {
   matches: [
     {
-      "match_id": 1,
-      "topic_icon": "/topic/icon/1/eb7bf9f2-62be-451c-af5b-5b41150eef1c.jpg",
-      "last_message": "Hi!",
-      "unread_messages": 2,
-      "user": {
-        "id": 1,
-        "full_name": "Jon Doe",
-        "avatar": {
-          "original_url": "/topic/icon/1/eb7bf9f2-62be-451c-af5b-5b41150eef1c.jpg",
-          "normal_url": "/topic/icon/1/eb7bf9f2-62be-451c-af5b-5b41150eef1c.jpg",
-          "small_thumb_url": "/topic/icon/1/eb7bf9f2-62be-451c-af5b-5b41150eef1c.jpg"
-        }
-      }
-    }
-  ]
+      match_id: 1,
+      topic_icon: '/topic/icon/1/eb7bf9f2-62be-451c-af5b-5b41150eef1c.jpg',
+      last_message: 'Hi!',
+      unread_messages: 2,
+      user: {
+        id: 1,
+        full_name: 'Jon Doe',
+        avatar: {
+          original_url:
+            '/topic/icon/1/eb7bf9f2-62be-451c-af5b-5b41150eef1c.jpg',
+          normal_url: '/topic/icon/1/eb7bf9f2-62be-451c-af5b-5b41150eef1c.jpg',
+          small_thumb_url:
+            '/topic/icon/1/eb7bf9f2-62be-451c-af5b-5b41150eef1c.jpg',
+        },
+      },
+    },
+  ],
 };
 
-mock.onGet(`${process.env.REACT_APP_API_HOST}/match_conversations`).reply(200, conversations);
+mock
+  .onGet(`${process.env.REACT_APP_API_HOST}/match_conversations`)
+  .reply(200, conversations);
 
 test('displays chats received from the server', async () => {
   render(<Chat />);
